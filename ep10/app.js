d524736e0b44c30c092440cc29be4e0f93f9f9d1
Jose Naves Moura Neto <josenaves@gmail.com>
2014-03-30 16:22:56 -0300

    Roteiro

--- ep10/app.js
+++ ep10/app.js
@@ -1,8 +1,33 @@
-var serviceApp = angular.module('ep09App', []);
+// DICA:
+// Usar SimpleHTTPServer como servidor http para testes
+//
+// jnaves@skynet:~/Dropbox/courses/angular-course/ep10$ python -m SimpleHTTPServer
+// Serving HTTP on 0.0.0.0 port 8000 ...
 
-serviceApp.service('statesService', function($http) {
-  // privado
-  var states = {};
+// necessário definir a dependência ngRoute
+var serviceApp = angular.module('ep10App', ['ngRoute']);
+
+ // Note: Providers can only be injected into config functions. Thus you could not inject $routeProvider into PhoneListCtrl. 
+serviceApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/states', { templateUrl: 'list.html', controller: 'controllerEp10'}).
+      when('/create', { templateUrl: 'create.html', controller: 'controllerEp10'}).
+      when('/delete', { templateUrl: 'list.html', controller: 'controllerEp10'}).
+      otherwise({
+        redirectTo: '/states'
+      });
+  }
+]);
+
+// habilita CORS 
+serviceApp.config(['$httpProvider', function($httpProvider) {
+  $httpProvider.defaults.useXDomain = true;
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+}]);
+
+
+serviceApp.service('statesService', function($http, $rootScope) {
   
   // público
   this.getLetters = function() {
@@ -14,34 +39,51 @@ serviceApp.service('statesService', function($http) {
   };
   
   this.addStateIntoCollection = function(estado, capital, renda) {
-    states.push({
+    // como acessar esta variável ?
+    $rootScope.states.push({
       name: estado,
       capital: capital,
       renda: renda
     });
   };
 
+  this.removeStateFromCollection = function(i) {
+    $rootScope.states.splice(i, 1);
+  };  
+
   this.getStates = function(callback) {
-    //$http.get('states.json').success(callback);
     $http.get('http://statesapi.apiary.io/app.apiary.io/states').success(callback);
   };
+
+});
+
+
+// Inicialização
+serviceApp.run ( function($rootScope, statesService) {
+  
+  // carrega os estados
+  statesService.getStates(function(data){
+    $rootScope.states = data;
+  });
+  
 });
 
 //--- AQUI VAI O CONTROLLER (agora mais magro)
-serviceApp.controller('controllerEp09', function($scope, statesService) {
+serviceApp.controller('controllerEp10', function($scope, $location, statesService) {
 
   $scope.filtro = "";
-
-  statesService.getStates(function(data){
-    $scope.states = data;
-  });
   
   $scope.today = statesService.getToday();
   $scope.letras = statesService.getLetters();
 
   $scope.addState = function() {
-    statesService.addStateIntoCollection($scope.estado, $scope.capital, 1000);  
-    $scope.estado = '';
-    $scope.capital = '';
+    statesService.addStateIntoCollection($scope.estado, $scope.capital, 1000); 
+    $location.path('#states');
   };
+
+  $scope.deleteState = function(index) {
+    statesService.removeStateFromCollection(index); 
+    $location.path('#states');
+  };
+
 });
