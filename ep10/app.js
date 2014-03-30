49cdb8845d49e240f16d8b84ed37c3f7c611a579
Jose Naves Moura Neto <josenaves@gmail.com>
2014-03-30 15:13:55 -0300

    Episódio 10 - routing

---
+++ ep10/app.js
@@ -0,0 +1,47 @@
+var serviceApp = angular.module('ep09App', []);
+
+serviceApp.service('statesService', function($http) {
+  // privado
+  var states = {};
+  
+  // público
+  this.getLetters = function() {
+    return "abcdefghijklmnopqrstuvwyz";
+  };
+  
+  this.getToday = function(){
+    return new Date();
+  };
+  
+  this.addStateIntoCollection = function(estado, capital, renda) {
+    states.push({
+      name: estado,
+      capital: capital,
+      renda: renda
+    });
+  };
+
+  this.getStates = function(callback) {
+    //$http.get('states.json').success(callback);
+    $http.get('http://statesapi.apiary.io/app.apiary.io/states').success(callback);
+  };
+});
+
+//--- AQUI VAI O CONTROLLER (agora mais magro)
+serviceApp.controller('controllerEp09', function($scope, statesService) {
+
+  $scope.filtro = "";
+
+  statesService.getStates(function(data){
+    $scope.states = data;
+  });
+  
+  $scope.today = statesService.getToday();
+  $scope.letras = statesService.getLetters();
+
+  $scope.addState = function() {
+    statesService.addStateIntoCollection($scope.estado, $scope.capital, 1000);  
+    $scope.estado = '';
+    $scope.capital = '';
+  };
+});
